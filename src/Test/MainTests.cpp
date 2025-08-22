10816b5ef09012cab87e078db7ad081abde3e8c6
Marc Palenzuela <marc.palenzuela.reyes@gmail.com>
2025-08-22 22:10:47 +0200

    wip ctest

---
+++ src/Test/MainTests.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "In main" << std::endl;
+    return 1;
+}
+
+bool FirstTest()
+{
+    std::cout << "In test" << std::endl;
+    return true;
+}
