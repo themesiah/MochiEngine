57465281f47a865eb5f48ad2ce67db4360bf3a12
Marc Palenzuela <marc.palenzuela.reyes@gmail.com>
2025-08-31 13:15:52 +0200

    New lua test with pointers

--- src/Core/Tests/LuaTest.cpp
+++ src/Core/Tests/LuaTest.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <iostream>
 #include <list>
+#include <memory>
 
 #include <sol/sol.hpp>
 
@@ -24,6 +25,76 @@ public:
     }
 
 public:
+    void SetName(const std::string &name)
+    {
+        Name = name;
+    }
+    const std::string &GetName()
+    {
+        return Name;
+    }
+    void Eat(const std::list<std::string> &foods)
+    {
+        std::cout << "The cat ate ";
+        for (auto f : foods)
+        {
+            std::cout << f << ", ";
+        }
+        std::cout << std::endl;
+    }
+    static void Speak(const std::string &w)
+    {
+        std::cout << "Miaaau~ " << w;
+    }
+
+private:
+    std::string Name;
+};
+
+class Cat2
+{
+public:
+    Cat2() : Name("Unnamed")
+    {
+    }
+
+    Cat2(const std::string &name) : Name(name)
+    {
+    }
+
+    virtual ~Cat2()
+    {
+    }
+
+public:
+    struct deleter
+    {
+        void operator()(Cat2 *p) const
+        {
+            destroy(*p);
+        }
+    };
+
+    static void Delete(Cat2 *p)
+    {
+        destroy(*p);
+    }
+
+    static std::shared_ptr<Cat2> create()
+    {
+        return std::shared_ptr<Cat2>(new Cat2("UNNAMED"), Delete);
+    }
+
+    static void initialize(Cat2 &uninitialized_memory)
+    {
+        new (&uninitialized_memory) Cat2();
+    }
+
+    static void destroy(Cat2 &memoryFromLua)
+    {
+        memoryFromLua.~Cat2();
+    }
+
     void SetName(const std::string &name)
     {
         Name = name;
@@ -71,4 +142,24 @@ TEST_CASE("LUA Class")
     lua.script("cat1:SetName(\"Cafe\");");
     CHECK(cat1.GetName() == "Cafe");
     //  lua.script_file(std::format("{}/Test.lua", CONST_DATA_FOLDER), sol::load_mode::text);
+}
+
+TEST_CASE("LUA Class pointer")
+{
+    sol::state lua;
+    lua.new_usertype<Cat2>("Cat2",
+                           "new", sol::initializers(&Cat2::initialize),
+                           "create", sol::factories(&Cat2::create),
+                           sol::meta_function::garbage_collect, sol::destructor(&Cat2::destroy),
+                           "GetName", &Cat2::GetName,
+                           "SetName", &Cat2::SetName);
+    lua.script("cat2 = Cat2.create();");
+    lua.script("cat2:SetName(\"Kirin\");");
+    std::shared_ptr<Cat2> &cat2 = lua["cat2"];
+    Cat2 *cat2ptr = lua["cat2"];
+    CHECK(cat2->GetName() == "Kirin");
+    CHECK(cat2ptr->GetName() == "Kirin");
+    lua.script("cat2:SetName(\"Cafe\");");
+    CHECK(cat2->GetName() == "Cafe");
+    CHECK(cat2ptr->GetName() == "Cafe");
 }
